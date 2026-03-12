987ec9258ad2870bbfe125508a9fdbc01ce39cdd
Nischal <nyyscal541556@gmail.com>
2026-03-12 11:02:24 +0545

    Deployment Changes

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,8 +1,8 @@
 import express from "express"
-import { syncUser } from "../controllers/userControllers.js"
+// import { syncUser } from "../controllers/userControllers.js"
 
 const router = express.Router()
 
-router.post("/sync", syncUser)
+// router.post("/sync", syncUser)
 
 export default router
